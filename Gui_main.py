51e6ccb5a3dc582b13c42b769b137a958ef7ff25
Kevin <kevinxz01@gmail.com>
2021-08-08 13:52:58 -0500

    Cleaning up gui_main.py file

--- Gui_main.py
+++ Gui_main.py
@@ -10,36 +10,7 @@ import webbrowser
 import urllib
 import io
 
-# class AnimeNews(tkinter.Frame):
-#     def __init__(self, master=None):
-#         super().__init__(master)
-#         self.master = master
-#         self.pack()
-#         self.create_widgets()
-#         self.create_anime_news_entry()
-
-#     def create_widgets(self):
-#         self.hi_there = tkinter.Button(self)
-#         self.hi_there["text"] = "Hello World\n(click me)"
-#         self.hi_there["command"] = self.say_hi
-#         self.hi_there.pack(side="top")
-
-#         self.quit = tkinter.Button(self, text="QUIT", fg="red",
-#                               command=self.master.destroy)
-#         self.quit.pack(side="bottom")
-
-#     def say_hi(self):
-#         print("hi there, everyone!")
-
-#     def create_anime_news_entry(self):
-#         test_frame = tkinter.LabelFrame(self)
-
-# if __name__ == "__main__":
-#     root = tkinter.Tk()
-#     app = AnimeNews(master=root)
-#     app.mainloop()
-
-
+# Turning image into byte in order to be used via ImageTK.
 def get_image_byte_io(image):
 
     image_url_open = urllib.request.urlopen(image)
@@ -51,7 +22,7 @@ def get_image_byte_io(image):
 
     return photo
 
-def populate_labels():
+def populate_labels(anime_news_url, anime_news_title, anime_news_body, anime_news_frame):
 
     anime_news_obj = News_Scraper.AnimeNews()
     news = anime_news_obj.individual_anime_news_cell()
@@ -63,7 +34,10 @@ def populate_labels():
     print (news)
 
     anime_news_url.config(text=news[0])
-    anime_news_title.config(text=news[1])
+    # anime_news_title.config(text=news[1])
+    anime_news_title.insert("end", news[1])
+    anime_news_title.config(state=DISABLED)
+
     # anime_news_body.config(text=news[2])
     anime_news_body.insert("end", news[2])
     # Makes tk.text read only.
@@ -75,6 +49,7 @@ def populate_labels():
     anime_news_image.image = image_photo
     anime_news_image.pack(side=LEFT, anchor=W, padx=10)
 
+# Taken from: https://stackoverflow.com/questions/51425633/tkinter-how-to-make-a-rounded-corner-text-widget
 def rounded_rect(canvas, x, y, w, h, c):
     canvas.create_arc(x,   y,   x+2*c,   y+2*c,   start= 90, extent=90, style="arc")
     canvas.create_arc(x+w-2*c, y+h-2*c, x+w, y+h, start=270, extent=90, style="arc")
@@ -85,41 +60,55 @@ def rounded_rect(canvas, x, y, w, h, c):
     canvas.create_line(x,   y+c, x,     y+h-c)
     canvas.create_line(x+w, y+c, x+w,   y+h-c)
 
-window = ThemedTk(theme="arc")
-window.geometry("400x700")
-window.title("Anime News")
-window.configure(bg="white")
-window.iconbitmap("favicon.ico")
+def template_frame_widget(frame_style, padding, frame_padx, frame_pady):
+    created_template = ttk.Frame(style=frame_style, padding=padding)
+    created_template.pack(padx=frame_padx, pady=frame_pady)
 
-style = ttk.Style()
+    return created_template
 
-borderImage = tk.PhotoImage("borderImage", data=border_frame_data.borderImageData)
-focusBorderImage = tk.PhotoImage("focusBorderImage", data=border_frame_data.focusBorderImageData)
+def main():
+    window = ThemedTk(theme="arc")
+    window.geometry("400x700")
+    window.title("Anime News")
+    window.configure(bg="white")
+    window.iconbitmap("favicon.ico")
 
-style.element_create("RoundedFrame",
-                     "image", borderImage,
-                     ("focus", focusBorderImage),
-                     border=16, sticky="nsew")
-style.layout("RoundedFrame",
-             [("RoundedFrame", {"sticky": "nsew"})])
+    # Changes the style of widget frame. Gives rounded edges for corners. 
+    style = ttk.Style()
 
-anime_news_frame = ttk.Frame(style="RoundedFrame", padding=10)
-anime_news_frame.pack(padx=15, pady=30)
+    borderImage = tk.PhotoImage("borderImage", data=border_frame_data.borderImageData)
+    focusBorderImage = tk.PhotoImage("focusBorderImage", data=border_frame_data.focusBorderImageData)
 
-# Clickable link label.
-anime_news_url = tk.Label(anime_news_frame, font=("Calibri Bold", 12), fg="blue", cursor="hand2", bg="white")
-anime_news_url.bind("<Button-1>",lambda event: webbrowser.open(anime_news_url.cget("text")))
-anime_news_url.pack(side=TOP, anchor=N, pady=10)
+    style.element_create("RoundedFrame",
+                        "image", borderImage,
+                        ("focus", focusBorderImage),
+                        border=16, sticky="nsew")
+    style.layout("RoundedFrame",
+                [("RoundedFrame", {"sticky": "nsew"})])
 
-anime_news_title = tk.Label(anime_news_frame,font=("Calibri Bold", 12), bg="white")
-anime_news_title.pack(side=TOP, pady=10, padx=8)
+    # anime_news_frame = ttk.Frame(style="RoundedFrame", padding=10)
+    # anime_news_frame.pack(padx=15, pady=30)
 
-number_of_screen_unit = 350
-# anime_news_body = tk.Label(anime_news_frame, font=("Calibri Bold", 10), wraplength=number_of_screen_unit, bg="white")
-anime_news_body = tk.Text(anime_news_frame, font=("Calibri Bold", 10), wrap="word", highlightthickness=0, borderwidth=0, height=10)
-#anime_news_body.pack(side=BOTTOM, pady=10, padx=8)
-anime_news_body.pack(side=BOTTOM, expand=True)
+    anime_news_frame = template_frame_widget("RoundedFrame", 10, 15, 30)
 
-populate_labels()
+    # Clickable link label.
+    anime_news_url = tk.Label(anime_news_frame, font=("Calibri Bold", 12), fg="blue", cursor="hand2", bg="white")
+    anime_news_url.bind("<Button-1>",lambda event: webbrowser.open(anime_news_url.cget("text")))
+    anime_news_url.pack(side=TOP, anchor=N, pady=10)
 
-window.mainloop()
+    # anime_news_title = tk.Label(anime_news_frame,font=("Calibri Bold", 12), bg="white")
+    anime_news_title = tk.Text(anime_news_frame, font=("Calibri Bold", 12), wrap="word", highlightthickness=0, borderwidth=0, height=1, bg="white")
+    anime_news_title.pack(side=TOP, pady=10, padx=8)
+
+    # number_of_screen_unit = 350
+    # anime_news_body = tk.Label(anime_news_frame, font=("Calibri Bold", 10), wraplength=number_of_screen_unit, bg="white")
+    anime_news_body = tk.Text(anime_news_frame, font=("Calibri Bold", 10), wrap="word", highlightthickness=0, borderwidth=0, height=10)
+    #anime_news_body.pack(side=BOTTOM, pady=10, padx=8)
+    anime_news_body.pack(side=BOTTOM, expand=True)
+
+    populate_labels(anime_news_url, anime_news_title, anime_news_body, anime_news_frame)
+
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import Gui_main
 
 if __name__ == "__main__":
     root = tkinter.Tk()
-    app = Gui_main.AnimeNews(master=root)
-    app.master.title("Anime News")
-    app.master.geometry("400x500")
+    # app = Gui_main.AnimeNews(master=root)
+    app = Gui_main.main()
+    app.geometry("400x700")
+    app.title("Anime News")
+    app.configure(bg="white")
+    app.iconbitmap("favicon.ico")
     app.mainloop()
